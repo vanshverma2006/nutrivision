ecfd54378f44ca835c05634f24c777c7976d9d81
vanshverma2006 <vansh.verma18082006@gmail.com>
2026-08-22 18:51:02 +0530

    chore: initialize NutriVision Spring Boot project

---
+++ src/main/java/com/nutrivision/NutriVisionApplication.java
@@ -0,0 +1,13 @@
+package com.nutrivision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutriVisionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NutriVisionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nutrivision/NutriVisionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nutrivision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutriVisionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
